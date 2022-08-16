d4328a7c3f8cbb762c78bb44f8c5c6ce5f2727fe
Cjiaye <11204115+cjiaye@user.noreply.gitee.com>
2022-08-16 15:31:50 +0800

    11

--- common/cart.js
+++ common/cart.js
@@ -1,10 +1,5 @@
 
 import Storage from "../utils/storage"
-
-/**
- * 将商品的数据添加到本地
- * @param {*} data 
- */
 const addCart = (data) => {
   const carsArray = []
   if(!hasLocalData()){
--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,20 +1,80 @@
 // pages/cart/cart.js
+import Storage from '../../utils/storage'
 Page({
+  //商品数量jia
+  handleCrement(event){
+    // console.log('jia')
+    // console.log('event',event)
+    const _index=event.currentTarget.dataset.index
+    console.log('_index',_index)
+    this.data.cartList[_index].num+=1
+    this.setData({
+      cartList:this.data.cartList
+    })
+    Storage.set('carts',this.data.cartList)
+  },
+  // 商品数量-
+  handleDrement(event){
+    const _index=event.currentTarget.dataset.index
+    console.log('_index',_index)
+    this.data.cartList[_index].num-=1
+    this.setData({
+      cartList:this.data.cartList
+    })
+    Storage.set('carts',this.data.cartList)
+  },
 
+  handleComputedCount(event,action){
+    const _index=event.currentTarget.dataset.index
+    action=='increment'?this.data.cartList[_index].num-=1:this.data.cartList[_index].num+=1
+    if(this.data.cartList[_index].num<=0){
+      this.data.cartList[_index].num=1
+      this.handleModalAction(_index)
+    }
+    this.setData({
+      cartList:this.data.cartList
+    })
+    Storage.set('carts',this.data.cartList)
+  },
+  handleModalAction(index){
+    wx.showModal({
+      title: '提示',
+      content: '您确定要删除此商品吗?',
+      success :(res) =>{
+        if (res.confirm) {
+          this.data.cartList.splice(index,1)
+          this.setData({
+            cartList : this.data.cartList
+          })
+          Storage.set("carts", this.data.cartList)
+          this.handleComputedPrice()
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })    
+  },
   /**
    * 页面的初始数据
    */
   data: {
-
+    cartList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-   
+    this.getCartList()
   },
-
+  getCartList(){
+    const cartList=Storage.get('carts')
+    // console.log('cartList',cartList)
+    if(cartList.length<0) return 
+    this.setData({
+      cartList
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -24,7 +84,7 @@ Page({
 
   /**
    * 生命周期函数--监听页面显示
-   */
+  / */
   onShow() {
 
   },
